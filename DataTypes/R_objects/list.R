3dacbe725b568462f969ec84d390fd7607cb9189
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-05-22 20:35:03 +0530

    vector and list examples

---
+++ DataTypes/R_objects/list.R
@@ -0,0 +1,16 @@
+points = c(2, 3, 1)
+axis = 'x-axis'
+result_list = list(points, axis, sin)
+
+print(result_list)
+
+# two vectors inside a list
+
+first_vec = c(2, 3, 6)
+second_vec = c(4, 5, 20)
+
+a_list = list(first_vec, second_vec)
+print(a_list)
+
+# class of a list
+print(class(a_list))
