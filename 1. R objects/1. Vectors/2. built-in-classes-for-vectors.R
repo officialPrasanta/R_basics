16bc4a0b4b06454079cbb03cc4eac99260603e54
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-05-23 01:06:15 +0530

    major fix: change in directories

---
+++ 1. R objects/1. Vectors/2. built-in-classes-for-vectors.R
@@ -0,0 +1,21 @@
+a_str = "A string"
+numeric_no = 23
+bool = TRUE
+complex_no = 3+1i
+integer_no = 20L
+
+
+#print data-type
+
+
+print(paste(a_str, "is a: ",  class(a_str), " class"))
+
+print(paste(n, "is a: ", class(numeric_no), " class"))
+
+print(paste(bool, "is a: ", class(bool), " class"))
+
+print(paste(complex_no, "is a: ", class(complex_no), " class"))
+
+print(paste(integer_no, "is a: ", class(integer_no), " class"))
+
+
---
+++ 1. R objects/3. Matrices/matrices_basic.R
@@ -0,0 +1,7 @@
+coordinates1 = c(3, 3, 0)
+coordinates2 = c(5, 2, -3)
+coordinates3 = c(8, 3, 5)
+
+# creating a matrix
+triangle = matrix(c(coordinates1, coordinates2, coordinates3), nrow = 3, ncol = 3)
+print(triangle)
