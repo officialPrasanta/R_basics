f985802906ca6d851d0ae71991d6229362bad7e9
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-05-23 08:49:17 +0530

    1. some old files updated/removed from old directories 2. updated built-in classes

--- 1. R objects/1. Vectors/2. built-in-classes-for-vectors.R
+++ 1. R objects/1. Vectors/2. built-in-classes-for-vectors.R
@@ -1,16 +1,17 @@
-a_str = "A string"
-numeric_no = 23
-bool = TRUE
-complex_no = 3+1i
-integer_no = 20L
+a_vec = c("This vector")
+a_str = "A string" # similar to c("A string")
+numeric_no = 23    # similar to c(23)
+bool = TRUE        # similar to c(TRUE)
+complex_no = 3+1i  # similar to c(3+1i)
+integer_no = 20L   # similar to c(20L)
 
 
 #print data-type
 
-
+print(paste(a_vec, "is a: ",  class(a_vec), " class"))
 print(paste(a_str, "is a: ",  class(a_str), " class"))
 
-print(paste(n, "is a: ", class(numeric_no), " class"))
+print(paste(numeric_no, "is a: ", class(numeric_no), " class"))
 
 print(paste(bool, "is a: ", class(bool), " class"))
 
