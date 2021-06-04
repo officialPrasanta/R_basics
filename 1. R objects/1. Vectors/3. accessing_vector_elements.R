3bcec72f793028e667e7c7615775345cba7aeb73
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-06-04 22:46:25 +0530

    new file

---
+++ 1. R objects/1. Vectors/3. accessing_vector_elements.R
@@ -0,0 +1,10 @@
+# accessing vector elements
+
+alphabets = LETTERS[1:10]
+
+print(alphabets)
+print(paste("class: ", class(alphabets)))
+
+# access the 1st element
+index = 1
+print(alphabets[index])
