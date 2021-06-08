9d8157eea22d4921cf1afd9b3cad28bd491e811c
Prasanta Roy Choudhury <officialprasantaroychoudhury@gmail.com>
2021-06-09 00:05:16 +0530

    codeblock for set working directory

--- 5_Plotting/3_Histogram/histogram.R
+++ 5_Plotting/3_Histogram/histogram.R
@@ -1,5 +1,19 @@
-# Create data for the graph
+# set the working directory in program
+# or set the current directory as working directory in R Studio: More-> Set As working directory
 
+working_dir = getwd()
+print(working_dir)
+
+tryCatch(
+  expr = {
+    current_dir = "./5_Plotting"
+    setwd(current_dir)
+  },
+  error = function(e){ 
+  }
+)
+
+# Create data for the graph
 pixels <-  c(207, 255, 245, 223, 177, 177, 87, 97, 177, 207, 123, 123, 207, 207, 255, 255, 87, 56, 56, 177, 245, 223, 245)
 
 # Give name to the chart
